7756af47b40b42c7e6bcb1168ddd18ca477fe0c1
yumium <yuming.zai.00@gmail.com>
2023-04-23 15:55:33 +0100

    Added binary tree iterative traversal

--- data-structures/binary-tree.py
+++ data-structures/binary-tree.py
@@ -5,22 +5,112 @@ class TreeNode:
         self.right = right
 
     @classmethod
-    def preOrder(node):
-        print(node.datum, end=" ")
+    def preOrder(cls, node):
+        print(node.datum, end='')
         if node.left:
-            preOrder(node.left)
+            cls.preOrder(node.left)
         #print(node.datum, end=" ")
         if node.right:
-            preOrder(node.right)
+            cls.preOrder(node.right)
         #print(node.datum, end=" ")
 
     @classmethod
-    def levelOrder(root):
+    def levelOrder(cls, root):
         q = [root]
         while len(q) > 0:
             n = q.pop()
-            print(n.val)
+            print(n.datum, end='')
             if n.left:
                 q.insert(0, n.left)
             if n.right:
                 q.insert(0, n.right)
+
+    @classmethod
+    def preOrderIter(cls, root):
+        stack = [None]  #
+        curNode = root  #
+
+        while curNode:  #
+            print(curNode.datum, end='')
+
+            if curNode.right:
+                stack.append(curNode.right)
+            
+            if curNode.left:
+                curNode = curNode.left
+            else:
+                curNode = stack.pop()
+
+    @classmethod
+    def inOrderIter(cls, root):
+        stack = []                          #
+        curNode = root                      #
+        while curNode or len(stack) > 0:    #
+            if curNode:                         #
+                stack.append(curNode)
+                curNode = curNode.left
+            else:                               #
+                curNode = stack.pop()
+                print(curNode.datum, end='')
+                curNode = curNode.right
+
+    @classmethod
+    def postOrderIter(cls, root):
+        stack = []                          #
+        curNode = root                      #
+        while curNode or len(stack) > 0:    #
+            while curNode:                      #
+                stack.append((curNode, 'L'))
+                curNode = curNode.left
+            curNode, mode = stack.pop()
+            if mode == 'L':                     #
+                stack.append((curNode, 'R'))
+                curNode = curNode.right
+            else:                               #
+                print(curNode.datum, end='')
+                curNode = None
+
+#     A
+#    / \
+#   B  C
+#  /|   \
+# D E    F
+#  /    / \
+# G    H   I
+
+my_tree = TreeNode("A",
+    TreeNode("B",
+        TreeNode("D", None, None),
+        TreeNode("E",
+            TreeNode("G", None, None),
+            None
+        )
+    ),
+    TreeNode("C", None, 
+        TreeNode("F",
+            TreeNode("H", None, None),
+            TreeNode("I", None, None)
+        )
+    )
+)
+
+if __name__ == '__main__':
+    print('PreOrder')
+    TreeNode.preOrderIter(my_tree)  # ABDEGCFHI
+    print()
+    
+    print('InOrder')
+    TreeNode.inOrderIter(my_tree)   # DBGEACHFI
+    print()
+
+    print('PostOrder')
+    TreeNode.postOrderIter(my_tree) # DGEBHIFCA
+    print()
+
+    print('Level Order')
+    TreeNode.levelOrder(my_tree)    # ABCDEFGHI
+    print()
+
+    print('PreOrder recursive')     # ABDEGCFHI
+    TreeNode.preOrder(my_tree)
+    print()
