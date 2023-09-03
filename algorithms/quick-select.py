b55e7d58421202f0fb5972b0466a996949021e4b
yumium <yuming.zai.00@gmail.com>
2023-09-03 14:19:08 +0200

    Cleaned up quickselect

--- algorithms/quick-select.py
+++ algorithms/quick-select.py
@@ -4,15 +4,17 @@ from partition import partition3
 def quickselect3(arr, i, j, k):
     '''
     Same as quickselect but this function is fine with non-distinct elements in `arr`
-    Pre: 1 <= k <= j-i; 0 <= i < j <= len(arr)
     Returns: The value of the k-th smallest element in arr[i..j); will also partition `arr` s.t. k smallest elements in arr[i..j) are in arr[i..i+k)
     '''
+    assert 0 <= i < j <= len(arr)
+    assert 1 <= k <= j-i
+
     # Randomise the pivot
     idx = random.randrange(i,j)
     arr[i], arr[idx] = arr[idx], arr[i]
 
     p, q = partition3(arr, i, j)
-
+    # arr[i..p) < pivot; arr[p..q) = pivot; arr[q..j) > pivot
     if p-i < k <= q-i:
         return arr[p]
     elif k <= p-i:
